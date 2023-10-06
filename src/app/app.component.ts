5bdd344f299538cb5c1985d7cadc472135351a41
Pelegrino <pelegrino@gmail.com>
2023-10-06 08:54:16 -0300

    NgModel e Formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapirest';
+
+  usuario = {login: '', senha: ''};
+
+  public login() {
+    console.log("Teste de login: " + this.usuario.login + " senha: " + this.usuario.senha)
+  }
 }
